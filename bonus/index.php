c4dd2b421e79d76bbe307082a4a661b46beea401
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-28 17:29:13 +0100

    bonus completed

--- bonus/index.php
+++ bonus/index.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Fragmentation</title>
 </head>
+
 <body>
-  
+
+  <?php
+
+  $paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elitr. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+  $subparagraphs = explode('.', $paragraph);
+
+  foreach ($subparagraphs as $subparagraph) {
+    echo $subparagraph ? "<p>$subparagraph.</p>" : "";
+  }
+
+  ?>
+
 </body>
+
 </html>
